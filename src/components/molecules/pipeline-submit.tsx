2364adbb56cb0058005a1262dbc4d57ab5298fc0
ullaskunder3 <ullaskunder3@gmail.com>
2025-08-19 12:52:20 +0530

    updaye button loading

--- src/components/molecules/pipeline-submit.tsx
+++ src/components/molecules/pipeline-submit.tsx
@@ -1,37 +1,46 @@
 "use client";
-import React from "react";
+import React, { useTransition } from "react";
 import { usePipelineStore } from "@hooks";
 import { Button } from "@atoms";
-import { Play } from "lucide-react";
+import { Play, Loader2 } from "lucide-react";
 import { parsePipeline } from "@api";
 import { analyzePipelineComplexity } from "@lib";
 
 const PipelineSubmit: React.FC = () => {
   const { getPipelineData: getStorePipelineData } = usePipelineStore();
+  const [isPending, startTransition] = useTransition();
 
-  const handleSubmit = async () => {
+  const handleSubmit = () => {
     const pipelineData = getStorePipelineData();
-    try {
-      console.log("getPipelineData", pipelineData);
-      const response = await parsePipeline(pipelineData);
-      const complexity = analyzePipelineComplexity(pipelineData);
-      alert(`
-        Pipeline Analysis:
-        Nodes: ${response.num_nodes}
-        Edges: ${response.num_edges}
-        Is DAG: ${response.is_dag}
-        Complexity: ${complexity.complexity}
-        Message: ${response.message}
-      `);
-    } catch (error: any) {
-      alert(`Error submitting pipeline: ${error.message}`);
-    }
+
+    startTransition(async () => {
+      try {
+        console.log("getPipelineData", pipelineData);
+        const response = await parsePipeline(pipelineData);
+        const complexity = analyzePipelineComplexity(pipelineData);
+
+        alert(`
+          Pipeline Analysis:
+          Nodes: ${response.num_nodes}
+          Edges: ${response.num_edges}
+          Is DAG: ${response.is_dag}
+          Complexity: ${complexity.complexity}
+          Message: ${response.message}
+        `);
+      } catch (error: any) {
+        alert(`Error submitting pipeline: ${error.message}`);
+      }
+    });
   };
 
   return (
-    <Button onClick={handleSubmit}>
-      <Play className="mr-2 h-4 w-4" />
-      Run Pipeline
+    <Button onClick={handleSubmit} disabled={isPending}>
+      {isPending ? (
+        <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+      ) : (
+        <Play className="mr-2 h-4 w-4" />
+      )}
+      {isPending ? "Running..." : "Run Pipeline"}
     </Button>
   );
 };
